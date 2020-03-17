93f4f37500fdccd96774c28e1a686110fdaef431
Donny <donny@DonnydeMacBook-Pro.local>
2020-03-17 08:45:37 +0800

    首次提交

---
+++ app/src/main/java/com/zds/doubleslideseekbardemo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.zds.doubleslideseekbardemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
